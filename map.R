277bed3b14f0b1a6f9561d028d36ac3f5c1af064
JessalynLim <62015914+JessalynLim@users.noreply.github.com>
2022-02-06 00:07:46 +1100

    Update map.R
    Changed the coordinates to match the new meeting location

--- map.R
+++ map.R
@@ -1,9 +1,9 @@
 library(leaflet)
 
 leaflet(option=leafletOptions(scrollWheelZoom=FALSE)) %>%
-  addMarkers(lng=145.16290, lat=-37.88022, label="Monash Rotaract") %>% 
+  addMarkers(lng=145.1196, lat=-37.9245, label="Monash Rotaract") %>% 
   addProviderTiles(providers$Esri.WorldGrayCanvas) %>% 
-  setView(lng=145.16290, lat=-37.88022, zoom = 17) %>% 
+  setView(lng=145.1196, lat=-37.9245, zoom = 17) %>% 
   
   htmlwidgets::appendContent(
     htmltools::tags$script(src = "static/Leaflet.Sleep.js")
