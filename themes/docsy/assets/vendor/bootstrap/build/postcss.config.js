038a6e5543d1b149cde785d3f5b27076ae07c513
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-04-01 00:27:04 +1100

    Rewrite without R/blogdown

--- themes/docsy/assets/vendor/bootstrap/build/postcss.config.js
+++ themes/docsy/assets/vendor/bootstrap/build/postcss.config.js
@@ -1,12 +1,18 @@
-module.exports = (ctx) => ({
-  map: ctx.file.dirname.includes('examples') ? false : {
-    inline: false,
-    annotation: true,
-    sourcesContent: true
-  },
-  plugins: {
-    autoprefixer: {
-      cascade: false
+'use strict'
+
+module.exports = ctx => {
+  return {
+    map: ctx.file.dirname.includes('examples') ?
+      false :
+      {
+        inline: false,
+        annotation: true,
+        sourcesContent: true
+      },
+    plugins: {
+      autoprefixer: {
+        cascade: false
+      }
     }
   }
-})
+}
